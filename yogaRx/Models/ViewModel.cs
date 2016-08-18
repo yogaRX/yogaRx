b7b5d7b9e692d6f5e5c489bd652c373a94cdf97e
Holly Rhodes <hllyrhodes@gmail.com>
2016-08-18 12:19:05 -0400

    details view, new view model

---
+++ yogaRx/Models/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yogaRx.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Pose> Poses { get; set; }
+        public IEnumerable<Ailment> Ailments { get; set; }
+
+    }
+}
