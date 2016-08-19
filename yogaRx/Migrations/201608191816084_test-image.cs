51aee8bfc5d305d87c72f448e7587d4bcb4b1bf5
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-08-19 14:55:38 -0400

    ok

---
+++ yogaRx/Migrations/201608191816084_test-image.cs
@@ -0,0 +1,16 @@
+namespace yogaRx.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class testimage : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ yogaRx/Migrations/201608191842496_idk.cs
@@ -0,0 +1,16 @@
+namespace yogaRx.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class idk : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
