0f3ff93640ff2cfbedb572273b921434f994fc99
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-08-29 09:58:46 -0400

    doot doot

---
+++ yogaRx/Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yogaRx.Models;
+
+namespace yogaRx.Controllers
+{
+ 
+        public class FileController : Controller
+        {
+            private ApplicationDbContext db = new ApplicationDbContext();
+
+            // GET: File
+            public ActionResult Index(int id)
+            {
+                var fileToRetrieve = db.Users.Find(id);
+
+                if (fileToRetrieve.PhotoBytes != null && fileToRetrieve.PhotoType != null)
+                    return File(fileToRetrieve.PhotoBytes, fileToRetrieve.PhotoType);
+
+                return View();
+            }
+        }
+    
+}
