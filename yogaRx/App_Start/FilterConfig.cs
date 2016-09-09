859d754a42af8bc132fea6f74f5af62f24311d0b
Holly Rhodes <Holly Rhodes>
2016-09-09 10:54:16 -0400

    https

--- yogaRx/App_Start/FilterConfig.cs
+++ yogaRx/App_Start/FilterConfig.cs
@@ -1,15 +1,50 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
+
+using RequireHttpsAttributeBase = System.Web.Mvc.RequireHttpsAttribute;
 
 namespace yogaRx
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class RequireHttpsAttribute : RequireHttpsAttributeBase
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+
+            if (filterContext.HttpContext.Request.IsSecureConnection)
+            {
+                return;
+            }
+
+            if (string.Equals(filterContext.HttpContext.Request.Headers["X-Forwarded-Proto"],
+                "https",
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            if (filterContext.HttpContext.Request.IsLocal)
+            {
+                return;
+            }
+
+            HandleNonHttpsRequest(filterContext);
+        }
+    }
+
     public class FilterConfig
     {
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new System.Web.Mvc.AuthorizeAttribute());
-            //filters.Add(new RequireHttpsAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
